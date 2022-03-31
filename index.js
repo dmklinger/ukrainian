04b02b0bda4b1ea909f0288a85b8adce47b1da5c
David Klinger <aviday.ingerklay@gmail.com>
2022-03-30 23:53:17 -0500

    third time's the charm?

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ var main = (data) => {
 		});
 }
 
-fetch('./words.json')
+fetch('words.json')
 	.then(res => res.json())
 	.then(out => { console.log(out); main(out); })
 	.catch(err => {throw err});
