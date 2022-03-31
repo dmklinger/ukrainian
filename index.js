7e6d36f9cb0bbf1b9ea21a70553e5296b83d4363
David Klinger <aviday.ingerklay@gmail.com>
2022-03-30 23:52:07 -0500

    maybe this will fix the path for github pages?

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ var main = (data) => {
 		});
 }
 
-fetch('ukrainian/words.json')
+fetch('./words.json')
 	.then(res => res.json())
 	.then(out => { console.log(out); main(out); })
 	.catch(err => {throw err});
