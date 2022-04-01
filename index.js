ffa9f54a2da0a19d35c52d4907f480ad94638d4e
David Klinger <aviday.ingerklay@gmail.com>
2022-04-01 14:44:23 -0500

    change from table to grid

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
 'use strict';
 
 var main = (data) => {
-	let data_sliced = data.slice(0, 300)
+	let data_sliced = data.slice(0, 1000)
 	let tr = d3.select("body")
-		.append('table')
-		.append('tbody').selectAll()
+		.append('div')
+			.attr('class', 'main')
+			.selectAll()
 		.data(data_sliced)
-		.join('tr')
+		.join('div')
+			.attr('class', 'row')
+
+	let gf = (o, form) => { if (form in o) { return o[form] }; return ['[no form]']; }  // get form
+
+	let single_noun_table = (obj, d) => {
+		// obj.text(JSON.stringify(d))
+		let word_data = [
+			[['nom.'], ['acc.'], ['gen.'], ['dat.'], ['ins.'], ['loc.'], ['voc.']],
+			[gf(d, 'nom n'), gf(d, 'acc n'), gf(d, 'gen n'), gf(d, 'dat n'), gf(d, 'ins n'), gf(d, 'loc n'), gf(d, 'voc n')]
+		]
+		/*
+		obj.selectAll()
+			.data(word_data)
+			.join('div')
+				.attr('class', 'row')
+				.selectAll()
+			.data((d) => { return d })
+			.join('div')
+				.attr('class', 'col')
+			.text((d) => { return d })
+		*/
+		obj.append('table').selectAll()
+			.data(word_data)
+			.join('tr').selectAll()
+			.data((d) => { return d })
+			.join('td').selectAll()
+			.data((d) => { return d })
+			.join('p')
+			.text((d) => { return d })
+	}
 
 	tr.selectAll()
 		.data((d) => { return [d.info ? [d.pos, `(${d.info})`] : [d.pos], d.word, d.freq, d.defs, d.forms]; })
-		.enter()
-		.append('td')
+			.enter()
+		.append('div')
+			.attr('class', 'col')
 		.each(function(d, i) {
 			let this_obj = d3.select(this)
 			switch (i) {
@@ -33,12 +65,35 @@ var main = (data) => {
 					break;
 				}
 				case 4: {
-					if (Object.keys(d).length > 0) { 
+					this_obj.attr('id', 'forms')
+					if ('nom n' in d || 'acc n' in d) {
+						single_noun_table(this_obj, d)
+					}
+					else if ('nom ns' in d || 'nom np' in d) {
+						this_obj.append('p')
+							.text('noun s/p form')
+					}
+					else if ('nom am' in d) {
+						this_obj.append('p')
+							.text('adj form')
+					}
+					else if ('inf' in d) {
+						this_obj.append('p')
+							.text('verb form')
+					}
+					else if (Object.keys(d).length > 0) { 
 						this_obj.text(JSON.stringify(d)) 
-					};
+					}
+					else {
+						this_obj.append('p')
+							.attr('id', 'indcl')
+							.append('i')
+							.text('indeclinable')
+							
+					}
 					break;
 				}
-				default: this_obj.text(d);
+				default: this_obj.append('p').text(d);
 			}
 		});
 }
