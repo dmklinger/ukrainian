c95bf1986cb2d38e9657dc0d7a97514e843fbb2f
David Klinger <aviday.ingerklay@gmail.com>
2022-04-17 21:22:08 -0500

    remove some incorrectly categorized as redundant definitions

--- etl/dictionary.py
+++ etl/dictionary.py
@@ -93,7 +93,16 @@ class Usage:
 		bad_defs = set()
 		for d1 in self.definitions.keys():
 			for d2 in self.definitions.keys():
-				if d1 != d2 and d1.lower() in d2.lower():
+				new_d = ''
+				parenthesis = 0
+				for d in d2:
+					if d == '(':
+						parenthesis += 1
+					if parenthesis == 0:
+						new_d += d
+					if d == ')':
+						parenthesis -= 1
+				if d1 != d2 and d1.lower() in new_d.lower():
 					bad_defs.add(d1)
 		for d in bad_defs:
 			del self.definitions[d]
